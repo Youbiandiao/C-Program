6d4b60fb95fc1d79d6c39a1fd2e7ceb5c5a7a3fc
Youbiandiao <x2772192501@163.com>
2022-04-11 21:02:05 +0800

    wdnmd

--- test.c
+++ test.c
@@ -7,6 +7,12 @@ int main()
     printf("wdnmd");
     printf("wdnmd");
     printf("wdnmd");
-
+    if(wnmd)
+    {
+        fot(int i=0;i<10;i++)
+        {
+            
+        }
+    }
     
 }
